c7602191795af5f01031ece4dca66bd7fddbe627
David Singleton <davidsingleton@gmail.com>
2014-01-20 16:50:53 -0800

    integrate github bridge

--- app.py
+++ app.py
@@ -4,20 +4,23 @@ import redis
 import requests
 from flask import Flask, Response, send_file
 from keyvalstore import KeyValStore
+from recipegithub import GitHubBridge
 
 app = Flask(__name__)
 app.debug = False
 
+GIT_REPO = 'https://api.github.com/repos/dps/recipes'
+
 je = json.JSONEncoder()
+github = GitHubBridge(GIT_REPO, requests)
 
 pubkeyval = KeyValStore(redis.StrictRedis(
     host='spadefish.redistogo.com', 
     port=9148, 
     db=0,
     password=os.environ['REDISKEY']),
-    'recipefe')
-
-GIT_REPO = 'https://api.github.com/repos/dps/recipes'
+    'recipefe',
+    github)
 
 @app.route('/api/recipe/<name>')
 def recipe(name):
